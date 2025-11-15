6b060ae5b8a33fb9075d5cf59acd4aa25e8fdca1
Reesi <reesiomar@gmail.com>
2025-11-15 20:30:25 +0400

    ref: temp disabled https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,9 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
+        /* if ($this->app->environment('production')) { */
+        /*     URL::forceScheme('https'); */
+        /* } */
     }
 
     /**
@@ -21,8 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
+        //
+        /* if ($this->app->environment('production')) { */
+        /*     URL::forceScheme('https'); */
+        /* } */
     }
 }
